c15f5a5bb42438992867b0f64b80453fac8b5f3f
kitamuraDev <masato1999824@icloud.com>
2025-09-23 15:55:47 +0900

    :sparkles: ベーシックなDO x WebSocketのサンプルを実装

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,46 @@
 import { DurableObject } from "cloudflare:workers";
 
 export class MyDurableObject extends DurableObject {
-	constructor(ctx: DurableObjectState, env: Env) {
-		super(ctx, env);
-	}
+  async fetch(request: Request): Promise<Response> {
+    const websocketPair = new WebSocketPair();
+    const [client, server] = Object.values(websocketPair);
 
-	async sayHello(name: string): Promise<string> {
-		return `Hello, ${name}!`;
-	}
+    this.ctx.acceptWebSocket(server);
+    return new Response(null, {
+      status: 101,
+      webSocket: client,
+    });
+  }
+
+  async webSocketMessage(ws: WebSocket, message: string | ArrayBuffer) {
+    ws.send(`[Durable Object] message: ${message}, connections: ${this.ctx.getWebSockets().length}`);
+  }
+
+  async webSocketClose(ws: WebSocket, code: number, reason: string, wasClean: boolean) {
+    console.log(`WebSocket closed: code=${code}, reason=${reason}, wasClean=${wasClean}`);
+  }
 }
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const stub = env.MY_DURABLE_OBJECT.getByName("foo");
-		const greeting = await stub.sayHello("world");
+    if (request.method === "GET" && request.url.endsWith("/websocket")) {
+      const upgradeHeader = request.headers.get("Upgrade");
+      if (!upgradeHeader || upgradeHeader !== "websocket") {
+        return new Response(null, {
+          status: 426,
+          statusText: "Durable Object expected Upgrade: websocket",
+          headers: {
+            "Content-Type": "text/plain",
+          },
+        });
+      }
 
-		return new Response(greeting);
+      const id = env.MY_DURABLE_OBJECT.idFromName("foo");
+      const stub = env.MY_DURABLE_OBJECT.get(id);
+
+      return stub.fetch(request);
+    }
+
+    return new Response("Hello World!");
 	},
 } satisfies ExportedHandler<Env>;
